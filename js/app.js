17f6debbddbf878c936fe0c0dca360d5b09c7e39
Baptiste <baptiste.boursin@gmail.com>
2020-04-06 10:59:47 +0200

    modif nav

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#toggle').click(function(){
+        $('.search').toggleClass('isOpen');
         $('.header').toggleClass('isOpen');
         $('.menu').toggleClass('isOpen');
     })
